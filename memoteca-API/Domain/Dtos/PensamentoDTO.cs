32db2d85c163d8ef41bf8874cf860bc51cac6cf3
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-15 17:26:07 -0300

    feat: Criar PensamentoDTO

---
+++ memoteca-API/Domain/Dtos/PensamentoDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Dtos;
+public class PensamentoDTO
+{
+    public string? Quote { get; set; }
+    public string? Autor { get; set; }
+    public int Modelo { get; set; }
+}
+
+public class UpdatePensamentoDTO
+{
+    public int Id { get; set; }
+    public string? Quote { get; set; }
+    public string? Autor { get; set; }
+    public int Modelo { get; set; }
+}
