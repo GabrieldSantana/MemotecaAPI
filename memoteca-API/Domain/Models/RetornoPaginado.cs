998eda7175c68331e3684e4a9257239cd703e4ab
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-16 15:49:08 -0300

    feat: Criar a model de retorno paginado

---
+++ memoteca-API/Domain/Models/RetornoPaginado.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+public class RetornoPaginado<T>
+{
+    public int TotalRegistros { get; set; }
+    public int Pagina { get; set; }
+    public int QtdPagina { get; set; }
+    public List<T> Registros { get; set; }
+}
