d066255975422233fe877b1c7f84a09861eb7b5f
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-16 13:53:15 -0300

    feat: Criar a interface do service do Quote

---
+++ memoteca-API/Application/Interfaces/IQuoteService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Application.Interfaces;
+public interface IQuoteService
+{
+    Task<TOutputModel> InserirQuoteAsync<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+        where TInputModel : class
+        where TOutputModel : class
+        where TValidator : AbstractValidator<QuoteModel>;
+    Task<TOutputModel> AtualizarQuoteAsync<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+        where TInputModel : class
+        where TOutputModel : class
+        where TValidator : AbstractValidator<QuoteModel>;
+    Task<List<QuoteModel>> ListarQuotesAsync();
+    Task<QuoteModel> RetornarQuoteAsync(int id);
+    Task<bool> RemoverQuoteAsync(int id);
+}
