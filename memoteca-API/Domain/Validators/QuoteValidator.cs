86dabdc2aeab5aae703bbe1f1d1d304982c1c614
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-16 12:45:18 -0300

    feat: Criar validações para campos da Quote

---
+++ memoteca-API/Domain/Validators/QuoteValidator.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Domain.Validators;
+public class QuoteValidator : AbstractValidator<Quote>
+{
+    public QuoteValidator()
+    {
+        RuleFor(x => x.Pensamento)
+            .NotNull().NotEmpty().WithMessage("O campo '{PropertyName}' é obrigatório!")
+            .MaximumLength(250).WithMessage("O campo '{PropertyName}' deve ter até {MaxLenght} caracteres!");
+
+        RuleFor(x => x.Autor)
+            .NotNull().NotEmpty().WithMessage("O campo '{PropertyName}' é obrigatório!")
+            .MaximumLength(50).WithMessage("O campo '{PropertyName}' deve ter até {MaxLenght} caracteres!");
+
+        RuleFor(x => x.Modelo)
+            .NotNull().WithMessage("O campo '{PropertyName}' é obrigatório!")
+            .InclusiveBetween(1, 3)
+            .WithMessage($"O campo '{{PropertyName}}' deve ser um modelo válido!");
+
+    }
+}
