5b28d05792f8738f4bea514067fb4fdc100918e7
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-16 13:37:29 -0300

    feat: Criar a interface do repositorio do quote

---
+++ memoteca-API/Infrastructure/Interfaces/IQuoteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Infrastructure.Interfaces;
+public interface IQuoteRepository
+{
+    Task<List<QuoteModel>> BuscarTodosQuotes();
+    Task<QuoteModel> BuscarQuoteId(int id);
+    Task<bool> InserirQuote(QuoteModel quote);
+    Task<bool> AtualizarQuote(QuoteModel quote);
+    Task<bool> ExcluirQuote(int id);
+}
