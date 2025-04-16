676980014101b1ae6a5764a4a6df9605e91baa24
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-16 14:30:05 -0300

    feat: Criar o AppDbContext

---
+++ memoteca-API/Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+public class AppDbContext : DbContext
+{
+    public DbSet<QuoteModel> Pensamentos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
