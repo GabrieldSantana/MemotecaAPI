660a0165533d4b00ed856f8cdd9ab5a5cb41707d
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-17 14:26:11 -0300

    fix: Ajustar propriedade

--- memoteca-API/Domain/Models/QuoteModel.cs
+++ memoteca-API/Domain/Models/QuoteModel.cs
@@ -4,7 +4,7 @@ namespace Domain.Models;
 public class QuoteModel
 {
     public int Id { get; set; }
-    public string Pensamento { get;set; }
+    public string Pensamento { get; set; }
     public string Autor { get; set; }
     public int Modelo { get; set; }
 }
