7e0fe734aa75dcbbb1a12834f06479b49b3e12af
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-17 14:25:42 -0300

    refactor: Excluir conteúdo do arquivo

--- memoteca-API/Infrastructure/Context/AppDbContext.cs
+++ memoteca-API/Infrastructure/Context/AppDbContext.cs
@@ -1,13 +1,1 @@
-﻿using Domain.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Infrastructure.Context;
-public class AppDbContext : DbContext
-{
-    public DbSet<QuoteModel> Pensamentos { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-    }
-}
+﻿
