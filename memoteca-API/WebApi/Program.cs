902ece07a3b430f1c168129c9987cb59a4caf45c
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-17 14:20:27 -0300

    feat: Injetar dependencias

--- memoteca-API/WebApi/Program.cs
+++ memoteca-API/WebApi/Program.cs
@@ -1,6 +1,34 @@
+using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+#endregion
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<IDbConnection>(provider => // Provedor abrindo a conexão
+{
+    // Estância do banco
+    SqlConnection connection = new SqlConnection(connectionString);
+    connection.Open();
+    return connection; // Retornando a conexão criada
+});
+
+#region Services
+builder.Services.AddScoped<IQuoteService, QuoteService>();
+#endregion
+
+#region Repositories
+builder.Services.AddScoped<IQuoteRepository, QuoteRepository>();
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +43,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll",
+//    policy => {
+//        policy.AllowAnyOrigin()
+//              .AllowAnyMethod()
+//              .AllowAnyHeader();
+//    });
+//});
+
+//app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
